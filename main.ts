c6fd79887f19c3c156ee59d418743272f96b6e87
sololim <101944276+sololim@users.noreply.github.com>
2022-03-24 20:32:19 -0400

    make face

--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
 input.onGesture(Gesture.SixG, function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . # . #
+        . # . # .
+        `)
 })
 basic.forever(function () {
     basic.showLeds(`
+        . # . # .
         . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        # . . . #
+        . # # # .
         . . . . .
         `)
 })
